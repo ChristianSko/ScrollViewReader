95eb6870a7d523e060b0a55ec99882c44f2dd1df
Christian Skorobogatow <christiansko@icloud.com>
2022-07-11 14:23:42 +0200

    feat(): add scrollviewreader

--- ScrollViewReader/ContentView.swift
+++ ScrollViewReader/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .padding(.leading)
+                .border(.black)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    
+                    Button("SCROLL NOW") {
+                            if let index = Int(textFieldText) {
+                                scrollToIndex = index
+                            }
+                    }
+                    
+                    ForEach(0..<50) {index in
+                        Text("This is item # \(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { value in
+                        withAnimation {
+                            proxy.scrollTo(value, anchor: .top)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
