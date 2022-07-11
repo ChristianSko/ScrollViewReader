1e98665e2741f468e751c6373a9d2e4cfde27c5c
Christian Skorobogatow <christiansko@icloud.com>
2022-07-11 14:01:29 +0200

    Initial Commit

---
+++ ScrollViewReader/ScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderApp.swift
+//  ScrollViewReader
+//
+//  Created by Christian Skorobogatow on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
